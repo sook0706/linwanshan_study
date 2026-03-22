2da8e9ea5600ad8c389d7248aadc29681d1d359c
sook0706 <3037058273@qq.com>
2026-03-22 22:30:32 +0800

    week01/project/DormRepairSystem directory

--- week01/project/DormRepairSystem/src/main/java/mapper/RepairOrderMapper.java
+++
@@ -1,8 +0,0 @@
-package com.dorm.mapper;
-import com.dorm.entity.RepairOrder;
-import org.apache.ibatis.annotations.Insert;
-
-public interface RepairOrderMapper {
-    @Insert("INSERT INTO repair_order (student_id, dorm_num, repair_content) VALUES (#{studentId}, #{dormNum}, #{repairContent})")
-    int insertRepairOrder(RepairOrder repairOrder);
-}
